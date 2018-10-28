73a66dbc87f862e4e01db868f4257fb3288437e4
Rustam Ibragimov <iamdaiver@gmail.com>
2018-10-28 22:05:03 +0300

    [gem] Timeline idea

---
+++ lib/sidekiq_scheduler_mock/job_runner/timeline.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+class SidekiqSchedulerMock::JobRunner
+  # @api private
+  # @since 0.1.0
+  class Timeline
+    # @return [?]
+    #
+    # @api private
+    # @since 0.1.0
+    attr_reader :initial_time
+
+    # @return [?]
+    #
+    # @api private
+    # @since 0.1.0
+    attr_reader :target_time
+
+    # @param initial_time [?]
+    # @param target_time [?]
+    # @return [void]
+    #
+    # @api private
+    # @since 0.1.0
+    def initialize(initial_time, target_time)
+      @initial_time = initial_time
+      @target_time  = target_time
+    end
+
+    # @return [Array<?>]
+    #
+    # @api private
+    # @since 0.1.0
+    def time_points
+      # ?
+    end
+  end
+end
