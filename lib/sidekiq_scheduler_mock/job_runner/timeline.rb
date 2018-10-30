e2c8d5158988864d1f84943d5b9d36ef15ee4bce
Rustam Ibragimov <iamdaiver@gmail.com>
2018-10-30 13:24:31 +0300

    [gem] consider the every_time too

--- lib/sidekiq_scheduler_mock/job_runner/timeline.rb
+++ lib/sidekiq_scheduler_mock/job_runner/timeline.rb
@@ -16,15 +16,23 @@ class SidekiqSchedulerMock::JobRunner
     # @since 0.1.0
     attr_reader :target_time
 
+    # @return [?]
+    #
+    # @api private
+    # @since 0.1.0
+    attr_reader :every_time
+
     # @param initial_time [?]
     # @param target_time [?]
+    # @param every_time [?]
     # @return [void]
     #
     # @api private
     # @since 0.1.0
-    def initialize(initial_time, target_time)
+    def initialize(initial_time, target_time, every_time)
       @initial_time = initial_time
       @target_time  = target_time
+      @every_time   = every_time
     end
 
     # @return [Array<?>]
